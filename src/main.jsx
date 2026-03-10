81ee7eb339839d4412d71b496c2ed44185f06990
Dylan Brown <browndy4890@gmail.com>
2026-03-10 13:44:50 -0600

    feat: add Vercel Analytics and Google Analytics 4

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import CommandCenter from './CommandCenter.jsx'
+import { inject } from '@vercel/analytics'
+
+inject()
 
 function Router() {
   const path = window.location.pathname;
